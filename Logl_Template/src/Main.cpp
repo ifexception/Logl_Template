a3c4b8bb6d9c0b715c0cd372b890d91c0e24f7ca
szymonwelgus <szymonwelgus@gmail.com>
2017-02-16 17:12:37 +0200

    updated for chapter 5 rectangle

--- Logl_Template/src/Main.cpp
+++ Logl_Template/src/Main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <array>
 
 #define GLEW_STATIC
 #include <GL/glew.h>
 
 #include "Window.h"
+
+const char* vertexShader = "#version 330 core\n"
+"layout (location = 0) in vec3 position;\n"
+"void main()\n"
+"{\n"
+"gl_Position = "
+"vec4(position.x, position.y, position.z, 1.0);"
+"}\n";
+
+const char* fragShader = "#version 330 core\n"
+"out vec4 color;\n"
+"void main()\n"
+"{\n"
+"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"}\n";
 
 int main(int argc, char* argv[])
 {
@@ -20,6 +36,85 @@ int main(int argc, char* argv[])
     window.GetWindowSize(width, height);
     glViewport(0, 0, width, height);
 
+    GLuint vShader = 0;
+    vShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vShader, 1, &vertexShader, nullptr);
+    glCompileShader(vShader);
+
+    GLint success = 0;
+    GLchar infoLog[512];
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vShader, 512, nullptr, infoLog);
+        std::cerr << "Error: Vertex shader compilation failure" << std::endl
+            << infoLog << std::endl;
+    }
+
+    GLuint fShader = 0;
+    fShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fShader, 1, &fragShader, nullptr);
+    glCompileShader(fShader);
+
+    memset(infoLog, 0, sizeof(infoLog));
+    glGetShaderiv(vShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vShader, 512, nullptr, infoLog);
+        std::cerr << "Error: Fragment shader compilation failure" << std::endl
+            << infoLog << std::endl;
+    }
+
+    GLuint shaderProgram = 0;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vShader);
+    glAttachShader(shaderProgram, fShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, nullptr, infoLog);
+        std::cerr << "Error: Shader Link failure" << std::endl
+            << infoLog << std::endl;
+    }
+
+    glDeleteShader(vShader);
+    glDeleteShader(fShader);
+
+    GLfloat vertices[] = {
+        0.5f,  0.5f, 0.0f,  // Top Right
+        0.5f, -0.5f, 0.0f,  // Bottom Right
+        -0.5f, -0.5f, 0.0f,  // Bottom Left
+        -0.5f,  0.5f, 0.0f   // Top Left
+    };
+    GLuint indices[] = {  // Note that we start from 0!
+        0, 1, 3,  // First Triangle
+        1, 2, 3   // Second Triangle
+    };
+
+    GLuint VBO, VAO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+    {
+        glBindBuffer(GL_ARRAY_BUFFER, VBO);
+        glBufferData(GL_ARRAY_BUFFER, sizeof vertices, vertices,
+            GL_STATIC_DRAW);
+
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof indices, indices,
+            GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+            static_cast<GLvoid*>(nullptr));
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+    }
+    glBindVertexArray(0);
+
+    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
     auto running = true;
     while (running) {
         window.PollEvents(running);
@@ -27,8 +122,17 @@ int main(int argc, char* argv[])
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glBindVertexArray(0);
+
         window.SwapWindow();
     }
+
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &VBO);
 
     return 0;
 }
